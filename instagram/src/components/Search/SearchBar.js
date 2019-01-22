cc1e0749f4d48531a1f5e84fe08c2f06f704468e
Raine Wallace <rainebeaudubs@gmail.com>
2019-01-22 11:47:26 -0500

    Got my search bar structured, not styled.

---
+++ instagram/src/components/Search/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './SearchBar.css'
+
+
+function SearchBar() {
+  return (
+    <div className='SearchBarDiv'>
+      <div className='LeftSearchBar'>
+        <div>
+          <i className="camera fas fa-camera-retro"></i>
+        </div>
+        <img src="https://techcrunch.com/wp-content/uploads/2014/06/instagram_topic.png?w=600" alt="instagram" />
+      </div>
+      <div className='CenterSearchBar'>
+        <input 
+          name="search-input" 
+          type="text" 
+          placeholder="Search">
+        </input>
+      </div>
+      <div classNAme='RightSearchBar'>
+        <i className="search-bar-icons far fa-compass"></i>
+        <i className="search-bar-icons far fa-heart"></i>
+        <i className="search-bar-icons far fa-user"></i>
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
